076936f3d876cc5061962427272c4b9acdf3c99d
NavalYemul <NavalYemul>
2023-09-27 06:29:49 +0000

    new

---
+++ (Clone) ETL Json.py
@@ -0,0 +1,36 @@
+# Databricks notebook source
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+df=spark.read.json("dbfs:/mnt/saunextadls/raw/json")
+
+# COMMAND ----------
+
+df1=df.withColumn("ingestiondate",current_timestamp()).withColumn("path",input_file_name())
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create schema if not exists json
+
+# COMMAND ----------
+
+df1.write\
+.mode("overwrite")\
+.option("path","dbfs:/mnt/saunextadls/raw/output/naval/json")\
+.saveAsTable("json.bronze")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select count(*) from json.bronze
+
+# COMMAND ----------
+
+# MAGIC %sql 
+# MAGIC select * from json.bronze
+
+# COMMAND ----------
+
+
